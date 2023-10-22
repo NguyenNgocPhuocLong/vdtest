be3eddff8e1e4d3897b248425e97b40f9f4afe15
NgocLong <nguyenngocphuoclong1994@gmail.com>
2023-10-22 11:32:57 +0700

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useVersionDetectHook } from '../lib/main'
 function App() {
   const [count, setCount] = useState(0)
-  const { loading } = useVersionDetectHook({
-    nameArg: "test",
-  });
-
-  console.log("loading", loading);
-
+ 
   return (
     <>
       <div>
